c0f2050fd693fe5801163ab8c3bf72c5f13707ce
Majoe <Tshepo.Majoe@standardbank.co.za>
2024-11-04 08:13:08 +0200

    adding 1st Python Program

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+#
+# Example File for HelloWorld
+#
+
+def main():
+    print("\nHello World")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name + "\n")
+    
+if __name__ == "__main__":
+    main()
