fff7c220e7f117ca02d67df50fb8347233119503
Majoe <Tshepo.Majoe@standardbank.co.za>
2024-11-04 12:28:58 +0200

    More on variable

---
+++ variablesCase.py
@@ -0,0 +1,8 @@
+variable  = "some value"
+print(variable)
+
+#variable in python are case senitive
+Variable = "some other value"
+print(Variable)
+VARIABLE = "another some other variable"
+print (VARIABLE)
