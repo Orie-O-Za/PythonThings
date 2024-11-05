fda552975b55c2d381159012442604207a7f70b7
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-05 19:02:17 +0200

    added second example

--- sequences.py
+++ sequences.py
@@ -5,4 +5,11 @@ print(name[0]) #To get the first letter
 print(name[1]) #To get the second letter
 print(name[2]) #To get the third letter
 print(name[3]) #To get the fourth letter
-
+print("  ")
+#Sequence example 2
+name2 = ["Orie", "Tumzie", "BieKay"]  #List
+print(name2)
+print("  ")
+print(name2[0]) #To get the first name
+print(name2[1])#To get the second name
+print(name2[2])#To get the third name
