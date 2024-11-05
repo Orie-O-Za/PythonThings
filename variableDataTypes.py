0cf89c022338f40844526bb81728140d3479df20
Majoe <Tshepo.Majoe@standardbank.co.za>
2024-11-05 07:44:54 +0200

    Added my personal Example

---
+++ variableDataTypes.py
@@ -0,0 +1,14 @@
+variable = 'hello'
+type(variable) #string
+
+variable1 = 25
+type(variable1) #intiger
+
+variable3 = 1.5
+type(variable3) #float
+
+variable4 = True
+type(variable4) #boolean *must be Capitalized
+
+variable5 = None
+type(variable5) #NoneType *Lack of a value
