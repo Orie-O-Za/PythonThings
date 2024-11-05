94013becfcea4ed57c668aa8368c921a12e475cc
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-05 17:54:55 +0200

    Learning Sequences

---
+++ sequences.py
@@ -0,0 +1,8 @@
+name = "Orie"
+print(name)
+print("  ")
+print(name[0]) #To get the first letter
+print(name[1]) #To get the second letter
+print(name[2]) #To get the third letter
+print(name[3]) #To get the fourth letter
+
