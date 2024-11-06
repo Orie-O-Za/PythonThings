3f51f409e49f76cc079223930b0f5bce6e9c088a
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-06 20:43:28 +0200

    More examples with lists

---
+++ lists.py
@@ -0,0 +1,12 @@
+# Define a list of names
+names =["Jeff", "Orie", "Tumzie", "BieKay"]
+
+print(names) #Prints the whole list
+print(names[2]) # Prints the third name
+
+names.append("Moolzar") #Adds Tumzie to the end of the list
+
+names.sort() #Sorts the list
+
+print(names)
+
