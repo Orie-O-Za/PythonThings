058d706732482f12babbc5b629bd2c4489ec14f2
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-06 21:22:55 +0200

    More exaples

--- loops.py
+++ loops.py
@@ -7,3 +7,15 @@ for i in [0,1, 2, 3, 4, 5]:
 print("Printing using a range ")
 for i in range(10):
     print(i)
+
+# A sequence example to print the elements in a list one element at a time
+print("Printing using a sequence ")
+names = ["Jeff", "Orie", "Tumzie", "BieKay"]
+for name in names:
+    print(name)
+
+# A character example to print the letters in a name one element at a time
+print("Printing one letter of a name one character at a time")
+name1 = "Orie"
+for character in name1:
+    print(character)
