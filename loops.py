f5fb05504ffffb1c73a1323d3129d7f113f3047d
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-06 21:11:24 +0200

    added range exaplme

--- loops.py
+++ loops.py
@@ -1,3 +1,9 @@
-# A simple for loop example to print the elements in a list
-for i in [1, 2, 3, 4, 5]:
+# A  for loop example to print the elements in a list one element at a time
+print("Printing using a for loop ")
+for i in [0,1, 2, 3, 4, 5]:
+    print(i)
+
+# A range example to print the elements in a list one element at a time
+print("Printing using a range ")
+for i in range(10):
     print(i)
