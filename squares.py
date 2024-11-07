8110287046a94d8e0314eb44f1804406cf77f0d4
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-07 23:34:51 +0200

    Adding another example

--- squares.py
+++ squares.py
@@ -2,3 +2,14 @@ from functions import square #Importing a function to correct the on purpose err
 
 for i in range(10):
     print(f"The square of {i} is {square(i)}")  
+
+
+
+#another way to achieve the same reult below
+
+"""
+import functions
+
+for i in range(10):
+    print(f"The square of {i} is {functions.square(i)}")
+"""
