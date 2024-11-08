d7302a6cb16abe0bfd218785543fc0fa65a1eb05
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-08 08:28:37 +0200

    Adding Classes.py

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Point():   #creating a class called Point
+    def __init__(self, x, y): #defining the class
+        self.x = x
+        self.y = y
+
+p = Point(3, 4)
+print(p.x) #prints the x value  
+print(p.y) #prints the y value
