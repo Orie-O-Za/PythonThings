de10eb3a1bf80a5fe0ec7bd37a9032f1618e4c67
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-08 10:06:41 +0200

    corrected mistake

--- decorators.py
+++ decorators.py
@@ -2,10 +2,11 @@ def announce(f):
     def wrapper():
         print("About to run the function...")
         f()
-        print("The function ran has completed!")
-
+        print("The functin ran has completed!")
     return wrapper
 
 @announce
 def hey():
     print("Hey Young World")
+
+hey()
