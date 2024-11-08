ba8c375b0dedaf890c43de152f18300dea7770fc
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-08 10:49:13 +0200

    spaces

--- decorators.py
+++ decorators.py
@@ -1,12 +1,17 @@
 def announce(f):
     def wrapper():
+
+        print("  ")
         print("About to run the function...")
+
         f()
         print("The functin ran has completed!")
+        print("  ")
     return wrapper
 
 @announce
 def hey():
-    print("Hey Young World")
+    print("Hey Young World!!! ")
 
 hey()
+ 
