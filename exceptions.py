b1e5c3a533bbbb13e7a9d6e91a58a2b4c4f1120b
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-09 23:44:56 +0200

    Added exception handling

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,12 @@
+import sys
+
 y = int(input("y: "))
 x = int(input("x: "))   
+try:
+    result = x / y
+       
+except ZeroDivisionError:
+    print("Can't divide by zero")           
+    sys.exit(1) 
 
-result = x / y
 print(f"{x} / {y} = {result}")
