a78da7169f6bd5fcdb35b262ceab3b4b909137ce
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-11 15:56:09 +0200

    added exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,13 +1,16 @@
 import sys                               #importing the sys module
 
-x = int(input("x: "))   
-y = int(input("y: "))
-
-try:                                    #try except method
+try:
+    x = int(input("x: "))   
+    y = int(input("y: "))
+except ValueError:
+    print("Error: Invalid input")        #exception handling for text inputs
+    sys.exit(1)
+try:                                     #try except method
     result = x / y
        
 except ZeroDivisionError:
-    print("Can't divide by zero")           
-    sys.exit(1)                         #exit the program with an error
+    print("Can't divide by zero")        #exception handling for division by zero
+    sys.exit(1)                          #exit the program with an error
 
 print(f"{x} / {y} = {result}")
