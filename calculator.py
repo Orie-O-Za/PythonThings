336f0f639b60d2812e844a6dbcff539fc65ef50f
Majoe <Tshepo.Majoe@standardbank.co.za>
2024-11-16 12:41:17 +0200

    Adding option methods

--- calculator.py
+++ calculator.py
@@ -2,10 +2,26 @@ from functions import *
 
 while True:
     print("what do you want to do?")
-        print("  ")
-        print("1 - Addition")
-        print("2 - Substraction")
-        print("3 - Multiplication")
-        print("4 - Division")
+    print("  ")
+    print("1 - Addition")
+    print("2 - Substraction")
+    print("3 - Multiplication")
+    print("4 - Division")
     print("Press the Q Key to Close The Calculator")
-    print("Enter your option : ")
+
+    choice = input("Enter your option : ")
+
+    num1 = float(input("Enter the 1st number:  "))
+    num2 = float(input("Enter the 2nd number:  "))
+
+    if choice == '1':
+        addition(num1,num2)
+    elif choice == '2':
+        subtraction(num1,num2)
+    elif choice == '3':
+        multiplication(num1,num2)
+    elif
+        choice == '4':
+        division(num1,num2)
+    else:
+        print("Invalid Choice")
