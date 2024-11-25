99ba0f9e9ce068c26f9b4b583178f2b9200741c1
Majoe <Tshepo.Majoe@standardbank.co.za>
2024-11-25 18:38:57 +0200

    Added example of comments

---
+++ comments.py
@@ -0,0 +1,14 @@
+print("  ")
+print("There is a single line comment below this line of text")
+# A single line code
+print("There is 1 example of block/muliti-line comment below this line as well")
+# 1st comment
+# 2nd comment
+# 3rd comment
+print("Theres another example of block/multi-line comment below this string")
+"""
+# This comment is another example of 
+# a block or multi-line
+# comment
+# .
+"""
