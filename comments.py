763c6f59ec772e98e7563c490e29c65dcae6bdca
Majoe <Tshepo.Majoe@standardbank.co.za>
2024-11-25 18:45:47 +0200

    done with comments

--- comments.py
+++ comments.py
@@ -7,8 +7,10 @@ print("There is 1 example of block/muliti-line comment below this line as well")
 # 3rd comment
 print("Theres another example of block/multi-line comment below this string")
 """
-# This comment is another example of 
-# a block or multi-line
-# comment
-# .
+This comment is another example of 
+a block or multi-line
+comment
+.
 """
+print(" ")
+#
