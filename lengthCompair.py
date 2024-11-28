790a5dc14a182e0fca7332fb69290c7ea696898a
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-28 23:51:13 +0200

    adding one more example to lengthCompair.py

--- lengthCompair.py
+++ lengthCompair.py
@@ -8,4 +8,8 @@ print(sumString == "Orie is Amazing")
 #another compairison example
 print(sumString != "Orie is Amazing") 
 #This will return false if the strings are equal and identical
-#...and true if the strings are not equal and identical
+#...and true if the strings are not equal and identical  
+
+print("code" not in sumString)
+#This will return false if the string is in the variable
+#...and true if the string is not in the variable
