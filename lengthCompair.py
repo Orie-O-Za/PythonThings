6baa751f754fa6c7938ad47edf44d1f8512298fb
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-28 22:44:54 +0200

    adding another example to lengthCompair.py

--- lengthCompair.py
+++ lengthCompair.py
@@ -4,3 +4,8 @@ print(len(sumString))
 print(sumString == "Orie is Amazing") 
 #This will return true if the strings are equal and identical
 #...and false if the strings are not equal and identical
+
+#another compairison example
+print(sumString != "Orie is Amazing") 
+#This will return false if the strings are equal and identical
+#...and true if the strings are not equal and identical
