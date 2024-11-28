f69401dc139f1d82054d5ac819bd9057073f8946
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2024-11-28 22:41:39 +0200

    adding lengthCompair.py

---
+++ lengthCompair.py
@@ -0,0 +1,6 @@
+sumString = "Orie is Amazing" 
+#Counting the number of characters in a string variable
+print(len(sumString))
+print(sumString == "Orie is Amazing") 
+#This will return true if the strings are equal and identical
+#...and false if the strings are not equal and identical
