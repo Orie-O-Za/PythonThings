4eed75ecbc1b2d34906564224a8558610468d039
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2025-04-14 14:10:05 +0200

    Code Added

---
+++ pyGames/pyQuiz.py
@@ -0,0 +1,42 @@
+questions = [
+
+{
+    "prompt": "What is the capital of France?",
+    "options": ["A. Paris", "B. London", "C. Berlin", "D. Madrid"],
+    "answer": "A",      
+}
+{
+    "prompt": "What is the largest planet in our solar system?",
+    "options": ["A. Earth", "B. Jupiter", "C. Saturn", "D. Neptune"],
+    "answer": "B",
+}
+{
+    "prompt": "What is the chemical symbol for gold?",
+    "options": ["A. Au", "B. Ag", "C. Pb", "D. Fe"],
+    "answer": "A",
+}
+{
+    "prompt": "Who wrote 'Romeo and Juliet'?",
+    "options": ["A. Charles Dickens", "B. William Shakespeare", "C. Mark Twain", "D. Jane Austen"],
+    "answer": "B",
+}
+{
+    "prompt": "What is the smallest prime number?",
+    "options": ["A. 0", "B. 1", "C. 2", "D. 3"],
+    "answer": "C",
+}
+{
+    "prompt": "In South Africa, putting meat of a fire grill is called...?",
+    "options": ["A. Braai", "B. Barbecue", "C. Grill", "D. Roast"],
+    "answer": "A",
+}
+]
+
+def start_quiz(questions):
+    score = 0
+    for questions in questions:
+        print(questions["prompt"])
+        for options in questions ["options"]
+            print(options)
+
+start_quiz(questions)
