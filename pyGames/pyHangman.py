23be83b1f085de87876554bd5daee11fb6385ded
Orie-G <57903255+Orie-O-Za@users.noreply.github.com>
2025-04-15 23:10:03 +0200

    Coding Hangman

---
+++ pyGames/pyHangman.py
@@ -0,0 +1,5 @@
+words = ["python","java", "javascript", "ruby", "html", "CSS", "php", "swift", "braai", "beer"]
+
+wordselected = random.chosen(words)
+wordshown = ['_' for _ in wordselected]
+chancetaken = 8
